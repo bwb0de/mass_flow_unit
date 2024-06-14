96c7eb3c7421336df6234ec6819481491e3bf0a5
Daniel Cruz <danielc@unb.br>
2024-06-14 20:22:27 -0300

    Criando classe inicial a partir da leitura do manual...

---
+++ mass_flow_unit.py
@@ -0,0 +1,141 @@
+import serial
+
+class MassFlowUnit:
+    def __init__(self, port_add, baud_rate, flux_max_v=100, timeout=1) -> None:
+        self.__mass_flow_unit = serial.Serial(port_add, baud_rate, timeout=1)
+        self.__flux_max_v = flux_max_v
+        self.__port_add = port_add
+        self.__baud_rate = port_add
+        
+        self.__alarm_st_translator = {
+            "D": "Disabled",
+            "N": "Normal",
+            "H": "High",
+            "L": "Low"
+        }
+        self.__totalizer_status_translator = {
+            "D": "Disabled",
+            "E": "Enabled",
+        }
+        self.__analog_output_translator = {
+            "0": "0-5 Vdc",
+            "1": "0-10 Vdc",
+            "2": "4-20 mA",
+        }
+        self.__mod_buss_translator = {
+            "0": "Installed",
+            "1": "Not installed",
+        }
+
+        # Testar, aparentemente com o código vigente o hex precisa possuir 4 digitos ex: '0xaaaa' verificar tamanho de retorno do equipamento
+        self.__alarm_events_register = {
+            0: "High Flow Alarm",
+            1: "Low Flow Alarm",
+            2: "Flow Between High and Low Limits",
+            3: "Totalizer#1 Exceed Set Event Volume Limit",
+            4: "Totalizer#2 Exceed Set Event Volume Limit",
+            5: "High Pressure Alarm",
+            6: "Low Pressure Alarm",
+            7: "Pressure between High and Low Limits",
+            8: "Low Temperature Alarm",
+            9: "Low Temperature Alarm",
+            10: "Temperature Between High and Low Limits",
+            11: "Pulse Output Queue overflow",
+            12: "Password Event (attempt to change password)",
+            13: "Power On Event (power on delay > 0)",
+        }
+
+        # Testar, aparentemente com o código vigente o hex precisa possuir 4 digitos ex: '0xaaaa' verificar tamanho de retorno do equipamento
+        self.__diagnostic_events_register = {
+            1: "CPU Temperature Too High",
+            2: "DP Sensor Initialization Error",
+            3: "AP Sensor Initialization Error",
+            4: "2.5 Vdc Reference Out of Range",
+            5: "Flow Out of Permissible Range",
+            6: "Absolute Pressure over Permissible Range",
+            7: "Gas Temperature Out of Range",
+            8: "Analog Output Alarm Flag",
+            9: "UART Serial Communication Error",
+            10: "Modbus Serial Communication Error",
+            11: "EEPROM R/W Error",
+            12: "Auto Zero Failure Flag",
+            13: "AP Tare Failure Flag",
+            14: "DP ADC Counts Invalid",
+            15: "AP ADC Counts Invalid",
+            16: "Fatal Error",
+        }
+
+    def verificar_conexao(self):
+        if self.__mass_flow_unit.is_open: print(f"MassFlow Device em {self.__port_add} at {self.__baud_rate} baud."); return
+        print(f"Falha ao conectar na porta {self.__port_add}.")
+
+    def enviar_comando(self, commando):
+        self.__mass_flow_unit.write(commando.encode())
+        self.__mass_flow_unit.flush()
+        response = self.__mass_flow_unit.readline().decode().strip()
+        return response
+
+    def __decodificar_valor_hexadecimal_de_alarm_or_diagnostic_ev_register(self, hex_value, alarm_event_mapping):
+        
+        #Verificar no manual
+        
+        """
+        Decodifica o valor hexadecimal com base no alarm_event_mapping.
+        
+        :param hex_value: Hexadecimal value as a string (e.g., '0x05')
+        :param event_mapping: Dictionary mapping bit positions to event descriptions
+        :return: List of active events
+        """
+        
+        # Convertendo de string_hex para binário
+        binary_value = bin(int(hex_value, 16))[2:].zfill(8)
+        print(binary_value)
+        binary_value = binary_value[::-1]
+        print(binary_value)
+        print("Size",len(binary_value))
+    
+        eventos_ativos = []
+        for bit_pos, event_desc in alarm_event_mapping.items():
+            print(bit_pos)
+            if binary_value[bit_pos] == '1':
+                eventos_ativos.append(event_desc)
+        
+        return eventos_ativos
+    
+    def decodificar_diagnostic_event(self, hex_value_str):
+        return self.__decodificar_valor_hexadecimal_de_alarm_or_diagnostic_ev_register(hex_value_str, self.__diagnostic_events_register)
+
+    def decodificar_alarm_event(self, hex_value_str):
+        return self.__decodificar_valor_hexadecimal_de_alarm_or_diagnostic_ev_register(hex_value_str, self.__alarm_events_register)
+
+
+    def obter_estado_equipamento(self):
+        r_di = self.enviar_comando("pi").split(',')
+        r_pi = self.enviar_comando("di").split(',')
+
+        mass_flow, volumetric_flow, total_meu_1, total_meu_2, gas_temp, gas_press, flow_alarm_st, temp_alarm_st, press_alarm_st, alarm_ev_register, diagnostic_ev_register = r_pi
+        gas_idx, gas_name, current_mass_eng_unit, current_volum_eng_unit, totalizer1_mode, totalizer2_mode, analog_output, mod_buss = r_di
+
+        resposta = {
+            "Mass Flow": float(mass_flow),
+            "Volumetric Flow": float(volumetric_flow),
+            "Total1 MEU": float(total_meu_1),
+            "Total2 MEU": float(total_meu_2),
+            "Temperatura Gás": float(gas_temp), #Firehaint?
+            "Pressão Gás": float(gas_press),
+            "Situação Alarme Flow": self.__alarm_st_translator[flow_alarm_st],
+            "Situação Alarme Temperatura": self.__alarm_st_translator[temp_alarm_st],
+            "Situação Alarme Pressão": self.__alarm_st_translator[press_alarm_st],
+            "Alarm Event Register": alarm_ev_register, #Tradutor hexcode
+            "Diagnostic Event Register": diagnostic_ev_register, #Tradutor hexcode
+            "Gás Index": gas_idx,
+            "Gás Name": gas_name,
+            "Current Mass Unit":current_mass_eng_unit,
+            "Current Volumetric Unit": current_volum_eng_unit,
+            "Totalizar 1 mode": self.__totalizer_status_translator[totalizer1_mode],
+            "Totalizar 2 mode": self.__totalizer_status_translator[totalizer2_mode],
+            "Analog Output": self.__analog_output_translator[analog_output],
+            "ModBuss": self.__mod_buss_translator[mod_buss],
+        }
+
+        return resposta
---
+++ notes.md
@@ -0,0 +1,164 @@
+1. To get currently selected Gas: !12,G<CR>
+The DPC will reply: !12,G:0,AIR<CR>
+(assuming the Current Gas is #0, calibrated for AIR)
+
+2. To get current Flow Rate Alarm status:
+!12,FA,R<CR>
+The DPC will reply: !12,FAR:N<CR> (assuming no flow alarm conditions)
+
+3. To get a mass and volumetric flow reading:
+!12,F<CR>
+The DPC will reply: !12,50.0,50.3<CR> (assuming the mass flow is at 50% FS)
+
+4. Set the Set Point to 100.0 % full scale:
+!12,SP,100.0<CR>
+The DPC will reply: !12,SP:100.0<CR>
+
+5. Set the High and Low Flow Alarm limit to 90% and 10% of Full Scale flow rate:
+!12,FA,C,90.0,10.0<CR>
+The DPC will reply: !12,90.00,10.00,<CR>
+
+
+Executar, uma vez, rotina para obter lista de gases configurados. Indice vai de [0-128] salvar essas valores em um dict/json.
+    Sequência comandos:
+        json = {}
+        for n in range(0,129):
+            G:n
+            return val G => json (inverter chave/valor para legibilidade humana; reinverter para enviar número à máquina)
+
+Criar rotina de setar gás conforme esses valores:
+    Select "Gas Name" from json. Get index as 'n'.
+    G:n
+    
+
+
+Apresentar todas essas informações na tela de escolha
+    Executar rotinas ao iniciar o script para anotar parâmetros do equipamento:
+        Comandos:
+            F   ·· Retorna dois valores  [testar se estes valores etão em PI, index <MF> e <VF>]
+              "Mass Flow","Volumétric Flow"
+
+            PI  ·· Retorna uma tupla de valores separado por vírgula
+              <MF>,
+              <VF>,
+              <Total#1 Value> [MEU],
+              <Total#2 Value> [MEU],
+              <Gas Temperature>,
+              <Gas Pressure>,
+              <Flow Alarm Status>,         [valores possíveis D,N,H,L; respectivamente Desativado, Normal, Alto e Baixo]
+              <Temp. Alarm Status>,        [valores possíveis D,N,H,L; respectivamente Desativado, Normal, Alto e Baixo]
+              <Press. Alarm Status>,       [valores possíveis D,N,H,L; respectivamente Desativado, Normal, Alto e Baixo]
+              <Alarm Events Register>,     hex
+              <Diagnostic Events Register> hex
+
+              Exemplo: 25.4,23.2,354.2,0.0,24.8,14.95,D,N,D,0x0,0x0
+            
+            DI  ·· Lê informações de configuração do dispositivo.
+              <Current Gas Idx>,
+              <Current Gas Name>,
+              <Full Scale Range> (L/min),
+              <Current MEU>,
+              <Current VEU>,
+              <Totalizer#1 mode>,       [valores podem ser E, para 'Enabled' ou D, para 'Disabled']
+              <Totalizer#2 mode>,
+              <Analog Output>,          [valores de 0-2, respectivamente: 0-5 Vdc, 0-10 Vdc, 4-20 mA]
+              <ModBus>                  [valores de 0 ou 1, respectivamente: Instalado, Não instalado]
+              
+              Exemplo: DI:5,Helium,0.200,Sml/min,ml/min,E,D,0,1
+
+
+
+Device info, obtem todas as informações do dispositivo
+    => DI
+
+Ler e escrever parâmetros de uma mistura de gases (p.108)
+    => CM [obtem a informação dos gases salvos na EEPROM] os índices de alocação variam entre [200-219], 20 slots
+    => 
+
+
+Valve control (p.104) <!!! Do not change these parameters. Consult factory technical support for more info.>
+    => V [sem arg, configurações correntes]
+    
+    => V<Arg>
+    
+    => VM:[n] =>
+        n = C => Fechado
+        n = A => Automático
+        n = O => Aberto
+
+    => VPR [restaura definições de fábrica]
+
+
+
+Definição das unidades de medida do fluxo de massa (Mass Flow):
+    => U [sem argumento, retorna a unidade corrente].
+
+Definição das unidades de medida do fluxo volumétrico (Volumetric Flow):
+    => VU [sem argumento, retorna a unidade corrente].
+
+
+Calibração (p.99)
+    => CS:<TempV>,<PressV>
+    => CS
+    => Definição de fábrica: S:70.0,14.696 [?] não seria CS:70.0,14.696
+
+
+
+
+
+Read EEPROM Memory
+    => MR<Arg>
+
+    O argumento varia entre 0 e 413 e o retorno é um valor de memória. Pode ser usado para faze um backup dos valores da EEPROM...
+    Se fizer essa operação, realizar com intervalos de consulta de 1 minuto. Na página 107 consta um aviso de risco de queima da EEPROM para
+    operações de escrita usando o comando Local Set Point Options
+
+
+
+UART Error Codes:
+1 – Command Not Supported or Back Door is not enabled.
+2 – Wrong# of Arguments
+3 – Address is Out of Range (MR or MW commands)
+4 – Wrong# of the characters in the Argument
+5 – Attempt to alter Write-Protected Area in the EEPROM
+6 – Proper Command or Argument not found
+7 – Wrong value of the Argument
+8 – Manufacturer-specific information EE access KEY (wrong key or key is disabled)
+
+
+Alarm Events codes and bit position:
+Code Event Description Bit position
+0 FLOW_ALARM_HIGH 0x0001
+1 FLOW_ALARM_LOW 0x0002
+2 FLOW_ALARM_RANGE 0x0004
+3 TOTAL1_HIT_LIMIT 0x0008
+4 TOTAL2_HIT_LIMIT 0x0010
+5 PRES_ALARM_HIGH 0x0020
+6 PRES_ALARM_LOW 0x0040
+7 PRES_ALARM_RANGE 0x0080
+8 TEMP_ALARM_HIGH 0x0100
+9 TEMP_ALARM_LOW 0x0200
+A TEMP_ALARM_RANGE 0x0400
+B PULSE_OUT_QUEUE 0x0800
+C PASSWORD_EVENT 0x1000
+D POWER_ON_EVENT 0x2000
+
+
+Diagnostic Events codes and bit position:
+Code Event Description Bit position
+0 CPU_TEMP_HIGH 0x0001
+1 DP EE INIT ERROR 0x0002
+2 AP EE INIT ERROR 0x0004
+3 VREF_OUT_OF_RANGE 0x0080
+4 FLOW ABOVE LIMIT 0x0010
+5 AP OUT OF RANGE 0x0020
+6 G TEMP OUT OF RANGE 0x0040
+7 ANALOG OUT ALARM 0x0080
+8 SER COMM FAILURE 0x0100
+9 MB COMM FAILURE 0x0200
+A EEPROM FAILURE 0x0400
+B AUTOZERO FAILURE 0x0800
+C AP TARE FAILURE 0x1000
+D DP PRESSURE INVALID 0x2000
+E AP PRESSURE INVALID 0x4000
+F FATAL_ERROR 0x8000
