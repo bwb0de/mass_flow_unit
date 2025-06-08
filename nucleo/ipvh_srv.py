223d9a8f54c7276d3f960d9b99ce7727dd2436c8
Daniel Cruz <danielc@unb.br>
2025-06-08 10:37:30 -0300

    Implementando log... Criando estratégia de registro para eliminar ipvh_dict...

--- nucleo/ipvh_srv.py
+++ nucleo/ipvh_srv.py
@@ -6,6 +6,8 @@ import time
 
 from pprint import pprint
 
+from .globals.paths import log_file
+
 pasta_dados_experimento = "C:\\Users\\Mauro\\Desktop\\Dados Experimentos"
 arquivo_dados_experimento = "C:\\Users\\Mauro\\Documents\\Devel\\mass_flow_unit\\config\\experimento.json"
 
@@ -36,27 +38,25 @@ ipvh = {'sensor_loop':
             'sensor_corrente': ""
         }
 
-def start_server(mostrar_dados_recebido=True):
+def start_server(mostrar_dados_recebido=True, host='127.0.0.1', port=11112, chunksize=1024):
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    host = '127.0.0.1'
-    port = 11112
     server_socket.bind((host, port))
     server_socket.listen(5)
     print(f"Servidor IPVH escutando em {host}:{port}")
     while True:
         client_socket, addr = server_socket.accept()
         print(f"conn nfo: {addr} => ", end="")
-        handle_client(diretorio_corrente_dados, client_socket)
+        handle_client(diretorio_corrente_dados, client_socket, chunksize=chunksize)
         
 
 
-def handle_client(diretorio_corrente_dados, client_socket) -> str:
+def handle_client(diretorio_corrente_dados, client_socket, chunksize=1024) -> str:
     global mostrar_dados_recebidos
     global contador_mensagens_recebidas
     contador_mensagens_recebidas += 1
 
     while True:
-        data = client_socket.recv(1024).decode().strip()
+        data = client_socket.recv(chunksize).decode().strip()
         if not data:
             break
         
@@ -66,10 +66,12 @@ def handle_client(diretorio_corrente_dados, client_socket) -> str:
         elif data == '@':
             if contador_mensagens_recebidas > 6:
                 contador_mensagens_recebidas = 0
-                for sensor in ['S1', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8']:
-                    ipvh['sensor_loop'][sensor]["primary"].append('@')
-                    ipvh['sensor_loop'][sensor]["secondary"].append('@')
-
+                with open(f"{diretorio_corrente_dados}\\dados_sensores.txt", 'a') as arquivo_dados_sensores:
+                    for sensor in ['S1', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8']:
+                       ipvh['sensor_loop'][sensor]["primary"].append('@')
+                       ipvh['sensor_loop'][sensor]["secondary"].append('@')
+                       arquivo_dados_sensores.write(f"[{time.ctime()}];{sensor};primary;@\n")
+                       arquivo_dados_sensores.write(f"[{time.ctime()}];{sensor};secondary;@\n")
             client_socket.send(" ".encode())
 
         instrucoes = data.strip().split(' ')
@@ -86,11 +88,23 @@ def handle_client(diretorio_corrente_dados, client_socket) -> str:
                 ipvh['sensor_loop'][sensor]["primary"].append(primario)
                 ipvh['sensor_loop'][sensor]["secondary"].append(secundario)
             except KeyError:
-                pass
-
+                with open(log_file, 'a') as f: f.write(f"[{time.ctime()}] => Sensor {sensor} inválido!\n")
+                
+            ### [1] Verificar se arquivo TXT [2] está criando log adequadamente, se estiver, apagar aqui
             with open(f"{diretorio_corrente_dados}\\dados_sensores.json", 'w') as arquivo_dados_sensores:
                json.dump(ipvh, arquivo_dados_sensores, indent=4)
 
+            ### [2]
+            #
+            # O objetivo dessa mudança é eliminar o dicionário acumulador de dados...
+            # Embora a estrutura seja conveniente, é possível que o longo tempo de trabalho esteja trazendo problemas 
+            # em função do consumo excessivo de memória... Além disso o programa tende a perder performance com o tempo
+            # devido ao grande volume de dados que é gravado de forma recorrente
+            #
+
+            with open(f"{diretorio_corrente_dados}\\dados_sensores.txt", 'a') as arquivo_dados_sensores:
+               arquivo_dados_sensores.write(f"[{time.ctime()}];{sensor};primary;{primario}\n")
+               arquivo_dados_sensores.write(f"[{time.ctime()}];{sensor};secondary;{secundario}\n")
             client_socket.send(" ".encode())
             
         elif instrucoes[0] == "get":
@@ -100,36 +114,30 @@ def handle_client(diretorio_corrente_dados, client_socket) -> str:
         
 
 
-def send_command(command):
+def send_command(command, host='127.0.0.1', port=11112, chunksize=1024):
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    host = '127.0.0.1'
-    port = 11112
     client_socket.connect((host, port))
     data_to_send = f'{command}'
     client_socket.send(data_to_send.encode())
-    data = client_socket.recv(1024).decode().strip()
+    data = client_socket.recv(chunksize).decode().strip()
     client_socket.close()
 
 
-def set_value(key, value):
+def set_value(key, value, host='127.0.0.1', port=11112, chunksize=1024):
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    host = '127.0.0.1'
-    port = 11112
     client_socket.connect((host, port))
     data_to_send = f'set {key} {value}'
     client_socket.send(data_to_send.encode())
-    data = client_socket.recv(1024).decode().strip()
+    data = client_socket.recv(chunksize).decode().strip()
     client_socket.close()
 
 
-def get_value(key):
+def get_value(key, host='127.0.0.1', port=11112, chunksize=1024):
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    host = '127.0.0.1'
-    port = 11112
     client_socket.connect((host, port))
     data_to_send = f'get {key}'
     client_socket.send(data_to_send.encode())
-    data = client_socket.recv(1024).decode().strip()
+    data = client_socket.recv(chunksize).decode().strip()
     client_socket.close()
     return data
 
@@ -150,13 +158,13 @@ def mudar_parametros_experimento():
     ano, mes, dia, hora, minuto = datetime.datetime.today().year, datetime.datetime.today().month, datetime.datetime.today().day, datetime.datetime.today().hour, datetime.datetime.today().minute
     os.chdir(pasta_dados_experimento)
     try: os.mkdir(nome_pesquisador)
-    except: pass
+    except: with open(log_file, 'a') as f: f.write(f"[{time.ctime()}] => Erro ao criar a pasta {nome_pesquisador}, em {pasta_dados_experimento} pasta já existe?\n")
     os.chdir(nome_pesquisador)
     try: os.mkdir(nome_substancia)
-    except: pass
+    except: with open(log_file, 'a') as f: f.write(f"[{time.ctime()}] => Erro ao criar a pasta {nome_substancia}, em '{pasta_dados_experimento}{os.sep}{nome_pesquisador}' pasta já existe?\n")
     os.chdir(nome_substancia)
     try: os.mkdir(f"{ano}_{mes}_{dia}-{hora}_{minuto}")
-    except: pass
+    except: with open(log_file, 'a') as f: f.write(f"[{time.ctime()}] => Erro ao criar a pasta {ano}_{mes}_{dia}-{hora}_{minuto}, pasta já existe?\n")
     os.chdir(f"{ano}_{mes}_{dia}-{hora}_{minuto}")
     global diretorio_corrente_dados
     diretorio_corrente_dados = os.getcwd()
